793e64e305f49c1410e9c50b02c346988f820ee9
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-28 22:53:15 +0600

    redme file add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
                             <p>{error}</p>
                         </div>
                         <div className="form-control mt-6">

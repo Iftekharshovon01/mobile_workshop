ac0818132a473f26c490539f649b20d2abc1f08e
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-28 22:45:37 +0600

    handle modal working

--- src/Pages/Modal/BookingModal.js
+++ src/Pages/Modal/BookingModal.js
@@ -53,26 +53,7 @@ const BookingModal = ({ modalProduct }) => {
         <>
             <input type="checkbox" id='bookmodal' className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box relative">
-                    <label htmlFor='bookmodal' className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold text-primary text-center">{productName}</h3>
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-8'>
-                        <p>Name</p>
-                        <input name='name' type="text" defaultValue={user?.displayName} readOnly className="input input-bordered w-full" />
-                        <p>email</p>
-                        <input name='email' type="email" defaultValue={user?.email} readOnly className="input input-bordered w-full" />
-                        <p>Product name</p>
-                        <input name='productName' type="text" defaultValue={productName} readOnly className="input input-bordered w-full" />
-                        <p>Price</p>
-                        <input name='price' type="text" defaultValue={resalePrice} readOnly className="input input-bordered w-full" />
-                        <p>phone</p>
-                        <input name='phone' type="text" placeholder="phone number" required className="input input-bordered w-full" />
-                        <p>meeting location</p>
-                        <input name='location' type="text" placeholder="meeting location" required className="input input-bordered w-full" />
-                        <br />
-                        <input className='btn btn-primary w-full' type="submit" value="Submit" />
-                    </form>
-                </div>
+
             </div>
         </>
     );
